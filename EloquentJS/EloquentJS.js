d136c477ab38bfa6dbb7e130e23fa36cff2bf93a
ninjaturtletavo <o.enriquez88@gmail.com>
2021-06-21 18:10:06 -0700

    update

--- EloquentJS/EloquentJS.js
+++ EloquentJS/EloquentJS.js
@@ -1,10 +1,33 @@
 //Intro
-let total =0;
+let total = 0;
 let count = 1;
 
 while (count <= 10) {
-    total += count;
-    count += 1;
+  total += count;
+  count += 1;
 }
 
 console.log(total);
+
+//Chapter 2
+// while loop
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+
+console.log(result);
+
+//for loop
+let forLoopResult = 1;
+for (
+  let forLoopCounter = 0;
+  forLoopCounter < 10;
+  forLoopCounter = forLoopCounter + 1
+) {
+  forLoopResult = forLoopResult * 2;
+}
+
+console.log(forLoopResult);
