40dcdd2b20a2c77883551db7494d9ea6efbfeead
ninjaturtletavo <o.enriquez88@gmail.com>
2021-06-29 15:50:08 -0700

    fizzbuzz completed

--- EloquentJS/EloquentJS.js
+++ EloquentJS/EloquentJS.js
@@ -32,7 +32,6 @@ for (
 
 console.log(forLoopResult);
 
-
 // Ch 2 Exercises
 //Write a loop that makes seven calls to console.log to output the following triangle
 /*
@@ -45,5 +44,25 @@ console.log(forLoopResult);
 #######
 */
 
-for (let line = "#"; line.length < 8; line += "#")
-  console.log(line);
+for (let line = "#"; line.length < 8; line += "#") console.log(line);
+
+/*
+FizzBuzz
+
+Write a progrm that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 
+(and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by
+both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
+
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+  if (i % 3 === 0) output += "Fizz";
+
+  if (i % 5 === 0) output += "Buzz";
+
+  if (i % 3 === 0 && i % 5 === 0) output += "FizzBuzz";
+
+  console.log(output || i);
+}
